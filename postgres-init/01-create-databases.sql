ced3201ef849b78fef3e755dcd5d24afae560489
alejandra.pinzon <ma.pinzons1@uniandes.edu.co>
2026-09-14 22:53:50 -0500

    Configuración docker compose

--- postgres-init/01-create-databases.sql
+++ postgres-init/01-create-databases.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE marketplace_db;
 CREATE DATABASE verificacion_db;
 CREATE DATABASE cotizacion_db;
+CREATE DATABASE notificaciones_db;
 GRANT ALL PRIVILEGES ON DATABASE marketplace_db TO hogar;
 GRANT ALL PRIVILEGES ON DATABASE verificacion_db TO hogar;
 GRANT ALL PRIVILEGES ON DATABASE cotizacion_db TO hogar;
+GRANT ALL PRIVILEGES ON DATABASE notificaciones_db TO hogar;
